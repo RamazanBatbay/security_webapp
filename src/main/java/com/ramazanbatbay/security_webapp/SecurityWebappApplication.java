e92e88252cd58cbd57738900409b2889a387bb2d
RamazanBatbay <batbara3374_aehit@students.vizja.pl>
2025-12-12 16:51:25 +0100

    Initial commit with SQLite configuration

---
+++ src/main/java/com/ramazanbatbay/security_webapp/SecurityWebappApplication.java
@@ -0,0 +1,13 @@
+package com.ramazanbatbay.security_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ramazanbatbay/security_webapp/SecurityWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramazanbatbay.security_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
